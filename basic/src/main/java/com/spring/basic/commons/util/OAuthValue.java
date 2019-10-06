579009ad69682ed0d0c5862a6203cd0b13d6a704
moonki <moonki88@naver.com>
2019-10-06 11:56:45 +0900

    첫번째 커밋입니다. 스프링 웹사이트입니다. ver 1.0 beta

---
+++ basic/src/main/java/com/spring/basic/commons/util/OAuthValue.java
@@ -0,0 +1,8 @@
+package com.spring.basic.commons.util;
+
+public interface OAuthValue {
+	
+	String KAKAO_APP_KEY = "b9a6421ab3e96d2c5b794217a450d9b4";
+	String KAKAO_REDIRECT_PATH = "/auth/kakao";
+	
+}
